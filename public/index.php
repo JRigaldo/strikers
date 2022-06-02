a3065ac74a4143b8608812b8b5b3fe2c2ed9bf63
Jeremy <rigaldo.jeremy@gmail.com>
2022-06-02 17:30:28 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$router = new App\Router(dirname(__DIR__) . '/views');
+
+$router
+    ->get('/blog', 'post/index', 'blog')
+    ->get('/blog/category', 'category/show', 'category')
+    ->run();
